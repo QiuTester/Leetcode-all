0e346a39440890aa21e7bebe329cf0f7f015fee2
QiuTester <92881486+QiuTester@users.noreply.github.com>
2022-08-08 12:08:29 +0800

    Create 面试01.07.py

---
+++ 面试01.07.py
@@ -0,0 +1,16 @@
+'''给你一幅由 N × N 矩阵表示的图像，其中每个像素的大小为 4 字节。请你设计一种算法，将图像旋转 90 度。
+
+不占用额外内存空间能否做到？'''
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        N = len(matrix)
+        # 矩阵顺时针翻转：先对角线翻转，再水平翻转
+        # 逆时针翻转：先水平翻转，再对角线翻转
+        # 不拆分成两步翻转的话，矩阵的值不是互换从而无法同时传递，导致混乱
+        for i in range(N):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for i in range(N):
+            matrix[i][:] = matrix[i][::-1]
