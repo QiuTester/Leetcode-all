514844e163e2d42f6f4902d8632290cfcd6f63b1
QiuTester <92881486+QiuTester@users.noreply.github.com>
2022-08-09 22:01:28 +0800

    Create 0167.py

---
+++ 0167.py
@@ -0,0 +1,21 @@
+'''给你一个下标从 1 开始的整数数组 numbers ，该数组已按 非递减顺序排列  ，请你从数组中找出满足相加之和等于目标数 target 的两个数。如果设这两个数分别是 numbers[index1] 和 numbers[index2] ，则 1 <= index1 < index2 <= numbers.length 。
+
+以长度为 2 的整数数组 [index1, index2] 的形式返回这两个整数的下标 index1 和 index2。
+
+你可以假设每个输入 只对应唯一的答案 ，而且你 不可以 重复使用相同的元素。
+
+你所设计的解决方案必须只使用常量级的额外空间。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。 '''
+
+# 双指针
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        low, high = 0, len(numbers)-1
+        while low < high:
+            sums = numbers[low] + numbers[high]
+            if sums > target: high -= 1          # 头+尾 > 目标值，尾缩短
+            elif sums < target: low += 1         # 头+尾 < 目标值，头缩短
+            else: return [low+1, high+1]
